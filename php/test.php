0de942ffd9d4f114c48581d9d7ed335dc7b58fbe
DynamiteStudio <mikailezgi25@gmail.com>
2023-11-22 10:50:50 +0100

    gj

---
+++ php/test.php
@@ -0,0 +1,14 @@
+<?php
+   //auteur: Zoheb
+   //functie: voorbeeld operatoren blz 4.36
+
+   $getal = 7000;
+   // optellen 1000 + 2000
+   $uitkomst = 1000 + 2000;
+   echo "Uitkomst = $uitkomst";
+
+   echo "<br>";
+   echo $uitkomst * 1.1;
+
+
+?>
