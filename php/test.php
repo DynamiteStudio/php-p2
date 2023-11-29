c94e4edbd650891b12c396edd6bf679ccabd8bb7
DynamiteStudio <mikailezgi25@gmail.com>
2023-11-29 08:59:52 +0100

    7tr

--- php/test.php
+++ php/test.php
@@ -2,13 +2,34 @@
    //auteur: Zoheb
    //functie: voorbeeld operatoren blz 4.36
 
-   $getal = 7000;
+   $getal = 2000;
    // optellen 1000 + 2000
-   $uitkomst = 1000 + 2000;
+   $uitkomst = 1000 + $getal;
    echo "Uitkomst = $uitkomst";
+
+
+
+   //formule: 3000 + 3000 * 10/100 = 10%
+   //formule: 3000 + 3000 * 0,1 = 10%
+   //formule: 3000 * 1,1
 
    echo "<br>";
    echo $uitkomst * 1.1;
+
+  $uitkomst = $uitkomst * 1.1;
+
+  // trek er 1000 vanaf
+  echo "<br>";
+  echo $uitkomst - 1000;
+
+
+
+
+ 
+
+
+
+
 
 
 ?>
